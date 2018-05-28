9924da19a0a99a43dfbd40bf879b60c5081c4347
HyojungPark <hj__park@naver.com>
2018-05-28 11:39:04 +0900

    Modify bigdata.html. Add three project overviews and side shows.

--- html/js/sub.js
+++ html/js/sub.js
@@ -1,21 +1,30 @@
 var slideIndex = 1;
-showSlides(slideIndex);
+showSlides(slideIndex,1);
+showMiniSlides(slideIndex,1);
+showSlides(slideIndex,2);
+showMiniSlides(slideIndex,2);
+showSlides(slideIndex,3);
+showMiniSlides(slideIndex,3);
 
 // Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n,m) {
+  showSlides(slideIndex += n,m);
+  showMiniSlides(slideIndex,m);
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
+function currentSlide(n,m) {
+  showSlides(slideIndex = n,m);
+  showMiniSlides(slideIndex,m);
 }
 
-function showSlides(n) {
+function showSlides(n,m) {
   var i;
-  var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("demo");
-  var captionText = document.getElementById("caption");
+  var container = document.getElementById("container"+m);
+  var slides =container.getElementsByClassName("mySlides");
+  var dots = container.getElementsByClassName("demo");
+ // var captionText = document.getElementById("caption");
+  
   if (n > slides.length) {slideIndex = 1}
   if (n < 1) {slideIndex = slides.length}
   for (i = 0; i < slides.length; i++) {
@@ -24,7 +33,35 @@ function showSlides(n) {
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
+  
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
-  captionText.innerHTML = dots[slideIndex-1].alt;
+  //captionText.innerHTML = dots[slideIndex-1].alt;
+}
+
+function showMiniSlides(n,m){
+  console.log("in showMiniSlides");
+  var container = document.getElementById("container"+m);
+  var miniSlides = container.getElementsByClassName("column");
+  var dots = container.getElementsByClassName("demo");
+
+  if (n > dots.length) {n = 1}
+  if (n < 1) {n = dots.length}
+
+  for (i = 0; i < dots.length ; i++) {
+    miniSlides[i].style.display = "none";
+  }
+  
+  var size = 6;
+  if(n <= 20 ){
+    size = 6;
+  }else{
+    size = dots.length - n;
+  }
+
+  for(i = 0 ; i <size ; i ++){
+    miniSlides[n+i-1].style.display = "block";
+  }
+
+  
 }
